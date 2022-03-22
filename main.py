49d5b7ba478a2e296b14cb4d2c43e40eefeca10e
vabhi2007 <70085064+vabhi2007@users.noreply.github.com>
2022-03-22 16:38:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import time
+
+try:
+    pygame.init()
+except False:
+    print("Fail")
+else:
+    print("Success")
+# Basic Colors
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+RightCharacter = pygame.image.load("Sprites/Right.png")
+LeftCharacter = pygame.image.load("Sprites/Left.png")
+
+DisplayX = 500
+DisplayY = 400
+Display = pygame.display.set_mode((DisplayX, DisplayY))
+Display.fill(white)
+
+GameObjects = []
+Object = (Display,(0,0,255),(50,50,100,100),2)
+GameObjects.append()
+pygame.draw.rect(Object[0],Object[1],Object[2])
+def RefreshEvn():
+    for Object in GameObjects:
+        pygame.draw.rect(Object)
+    pygame.display.update()
+while True:
+    RefreshEvn()
+    pass
